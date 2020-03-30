1006a7d905f6d07e6c75c6d7a99b1fb72b46457b
Joan Garcia <joangarcia2015@gmail.com>
2020-03-29 21:36:49 -0600

    Clases Moore.

--- SimplificacionAutomatas/src/simplificacionautomatas/Simplifica.java
+++ SimplificacionAutomatas/src/simplificacionautomatas/Simplifica.java
@@ -3,15 +3,22 @@ package simplificacionautomatas;
 import estructura.Automata;
 
 public class Simplifica {
+    Moore equivalencia;
   public Simplifica(){
-    
+    equivalencia = new Moore();
   }
   
   public void algoritmo(Automata a){
     Automata b = a;
-    
+    String[] estados;
+    estados = a.getEstados();
     for (int i = 0; i < a.getEstados().length; i++) {
+      a.setEstadoInicial(estados[i + 1]);
+      b.setEstadoInicial(estados[i + 2]);
       
+      if (equivalencia.algoritmo(a, b)) {
+        
+      }
     }
   }
 }
