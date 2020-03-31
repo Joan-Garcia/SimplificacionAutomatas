91e96d90cbce73c599f8046e51d3d4552323f4ad
Joan Garcia <joangarcia2015@gmail.com>
2020-03-31 13:42:07 -0600

    Versión preliminar.

--- SimplificacionAutomatas/src/simplificacionautomatas/Simplifica.java
+++ SimplificacionAutomatas/src/simplificacionautomatas/Simplifica.java
@@ -13,26 +13,16 @@ public class Simplifica {
       b = new Automata(a);                                                      // Crea un máquina Mb
       
       for (int j = 0; j < b.getEstados().length; j++) {                         // Recorre cada estado de Mb...
-        if(a.getEstados()[i].equals(b.getEstados()[j])){                        // Menos el mismo estado.
-          // No hagas nada.                                                     // Si es el mismo, pasa al siguiente.
-        }else{
+        if(!a.getEstados()[i].equals(b.getEstados()[j])){                        // Menos el mismo estado. Si es el mismo, pasa al siguiente.
           b.setEstadoInicial(b.getEstados()[j]);
-                      
-//          System.out.println("Compara la equivalencia de Ma: "+a.getEstadoInicial()+
-//                " Mb: "+ b.getEstadoInicial());
-//          System.out.println("Con estados finales de Ma: "+a.getEstadosFinales()[0]+
-//                " Mb: "+ b.getEstadosFinales()[0]);
-//            
-          if(new Moore().algoritmo(a, b)){                                      // Si los autómatas son equivalentes.
-//            System.out.println("EliminaEstado Ma: "+a.getEstadoInicial()+
-//                    " Mb: "+b.getEstadoInicial());
-          a.eliminaEstado(a.getEstadoInicial(), b.getEstadoInicial());          //    Elimina de Ma el estado de Mb.
-          }  
-        }   
+          if(new Moore().algoritmo(a, b))                                       // Si los autómatas son equivalentes.
+            a.eliminaEstado(a.getEstadoInicial(), b.getEstadoInicial());        // Elimina de Ma el estado de Mb.
+        }
       } 
       if((i + 1) != a.getEstados().length)                                      // Si hay un estado siguiente.
         a.setEstadoInicial(a.getEstados()[i + 1]);                              // Analiza el siguiente estado.
-    }  
+    }
+    a.setEstadoInicial(a.getEstados()[0]);
     return a;
   }
 }
